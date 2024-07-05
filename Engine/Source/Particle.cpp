6b1b5cf4d9a46b653059ff2a9b947db1df18e595
DaikwonR <157632694+DaikwonR@users.noreply.github.com>
2024-07-05 11:34:57 -0600

    Changes to stars

--- Engine/Source/Particle.cpp
+++ Engine/Source/Particle.cpp
@@ -11,4 +11,10 @@ void Particle::Update(float dt)
 void Particle::Draw(Renderer renderer)
 {
     renderer.DrawPoint(position.x, position.y);
+
+    uint8_t r = rand() % 255 + 1;
+    uint8_t g = rand() % 255 + 1;
+    uint8_t b = rand() % 255 + 1;
+    uint8_t a = rand() % 255 + 1;
+    renderer.SetColor(r, g, b, a);
 }
